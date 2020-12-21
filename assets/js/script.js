ad01f45e6a5398a532740b582629fa4eae13735b
MWPX777 <mwpx777@gmail.com>
2020-12-21 09:13:43 -0600

    updated JS to enter text and item selected from dropdown box

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,31 @@
 
  //this function must come before the event listener
  var createTaskHandler = function(event){
+   //this will stop browser from refreshing
     event.preventDefault();
-    listItemEl = document.createElement("li");
+    //this line finds the HTML where text is inputted
+    const taskNameInput = document.querySelector("input[name='task-name']").value;
+    //this finds which dropdown item is selectd from the list
+    const taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    //create list item
+    const listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
+
+    //create div to hold task info and add to list item
+    const taskInfoEl = document.createElement("div");
+
+    //give it a class name
+    taskInfoEl.className = "task-info";
+
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class = 'task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-    console.log(event);
+    
 }
 
 //this is a function   event is the function
@@ -21,16 +40,6 @@ formEl.addEventListener('submit', createTaskHandler);
 //1 click on button
 //2 li item will be created 
 //3 li item will take on CSS style of task-item
-//4 textContent will add "This is a new task" text 
-//5 appendChild will add listItemEl to end of tasksToDoEl
+//4 textContent will add taskNameInput (user entered text) 
+//5 appendChild will add listItemEl to tasksToDoEl
 
-//tasksToDoEl will equal id = tasks-to-do
-//var tasksToDoEl = document.querySelector("#tasks-to-do");
-//taskItemEl will equal li element
-//var taskItemEl = document.createElement("li");
-//textContent will set text
-//taskItemEl.textContent = "Tasks To Do";
-//className will give the list item CSS properties
-//taskItemEl.className = "task-item";
-//appendChild will add taskItemEl to list taskToDoEl at bottom of list
-//tasksToDoEl.appendChild(taskItemEl);
